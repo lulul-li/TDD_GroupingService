67ce48c3051a7a09d7882419a3e22f4db36638a7
lulu <s987337613@gmail.com>
2017-06-14 00:26:52 +0800

    rename

---
+++ TDD_GroupingService/GroupingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TDD_GroupingService
+{
+    public class GroupingService
+    {
+
+        public List<int> Grouping(int groupingNumber, string sumColumn, object item)
+        {
+           return new List<int>();
+        }
+
+        static void Main()
+        {
+        }
+    }
+}
---
+++ TDD_GroupingService/Product.cs
@@ -0,0 +1,14 @@
+namespace TDD_GroupingService
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public decimal Cost { get; set; }
+        
+        public decimal Revenue	{ get; set; }
+
+        public decimal SellPrice { get; set; }
+
+    }
+}
