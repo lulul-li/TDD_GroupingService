1e4125ce25d80fcbd0c37dbf6810d5e3d5faa907
lulu <s987337613@gmail.com>
2017-06-14 00:15:18 +0800

    add grouping service test

---
+++ TDD_homework/TDD_homework/GroupingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TDD_homework
+{
+    public class GroupingService
+    {
+
+        public List<int> Grouping(int groupingNumber, string sumColumn, object item)
+        {
+           return new List<int>();
+        }
+
+        static void Main()
+        {
+        }
+    }
+}
